8cbde1cfc358f0ac522aca7cb015203fd52b4db5
Craig-WIT <20095351@mail.wit.ie>
2025-09-02 14:52:57 +0100

    Updating Naviagtion and creating home page contents

---
+++ src/routes/(marketing)/blog/[id]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from "$app/state";
+
+</script>
+<h1 class="text-4xl">Single Blog: {page.params.id}</h1>
+
+<a href="/blog/123">Another Text</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-<nav class="my-2 bg-amber-600 p-2">
-	<ul class="flex gap-2">
-		<li>
-			<a href="/">Home</a>
-			<a href="/about">About</a>
-		</li>
-	</ul>
-</nav>
-
-{@render children?.()}
+{@render children()}
