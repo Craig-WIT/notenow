a75cb23b46a5b3644da8bfbf06a70883101f137e
Craig-WIT <20095351@mail.wit.ie>
2025-09-02 16:28:44 +0100

    Awaiting parent load functions

--- src/routes/(marketing)/blog/+page.server.ts
+++ src/routes/(marketing)/blog/+page.server.ts
@@ -1,9 +1,11 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({parent}) => {
+    const parentData = await parent();
     console.log('Blog Route Universal Load')
     return {
         title: "The Blog",
-        count: 10
+        count: 10,
+        parentData: parentData,
     };
 }) satisfies PageServerLoad;
