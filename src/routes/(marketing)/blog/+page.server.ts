4e3e11b09feb798a667845537b59c902d4937348
Craig-WIT <20095351@mail.wit.ie>
2025-09-03 15:28:16 +0100

    Introduction to pagination

--- src/routes/(marketing)/blog/+page.server.ts
+++ src/routes/(marketing)/blog/+page.server.ts
@@ -1,10 +1,11 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import type { PostsResponse } from '$lib/types';
+import { POSTS_PER_PAGE } from '$lib/constants';
 
-export const load = (async ({fetch}) => {
-    
-const postRes = await fetch('/api/posts');
+export const load = (async ({fetch, url}) => {
+const page = +(url.searchParams.get('page') || 1)
+const postRes = await fetch(`https://dummyjson.com/posts?limit=${POSTS_PER_PAGE}&skip=${(page - 1) * POSTS_PER_PAGE}`);
 if(!postRes.ok) {
     error(postRes.status, 'An error has occured!')
 }
