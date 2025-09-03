d734e8b57dd2d0415063f53f40a2cb05eb4efbed
Craig-WIT <20095351@mail.wit.ie>
2025-09-03 11:32:34 +0100

    Creating GET endpoint

---
+++ src/routes/api/posts/+server.ts
@@ -0,0 +1,8 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({fetch}) => {
+    const postsRes = await fetch('https://dummyjson.com/posts');
+    const postResJSON = await postsRes.json();
+    return json(postResJSON, {status:200})
+};
