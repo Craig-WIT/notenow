eb1d3d3ec9062f4a62d7663f27a2575647fa798b
Craig-WIT <20095351@mail.wit.ie>
2025-09-03 11:46:40 +0100

    Creating POST endpoint

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,8 +1,19 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({fetch}) => {
-    const postsRes = await fetch('https://dummyjson.com/posts');
-    const postResJSON = await postsRes.json();
-    return json(postResJSON, {status:200})
+    const postRes = await fetch('https://dummyjson.com/posts');
+    const postResJSON = await postRes.json();
+    return json(postResJSON, {status: postRes.status})
+};
+
+export const POST: RequestHandler = async ({request}) => {
+    const post = await request.json();
+    console.log(post);
+
+    if(!post.title){
+        error(400, 'No post title')
+    }
+
+    return json({id: crypto.randomUUID(), title: post.title});
 };
