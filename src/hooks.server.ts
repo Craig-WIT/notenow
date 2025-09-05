bc92993c27625a98c35497d8f00e96c4c229319f
Craig-WIT <20095351@mail.wit.ie>
2025-09-05 08:57:29 +0100

    Using locals for authentication

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,14 @@
-import type { Handle } from "@sveltejs/kit";
+import { redirect, type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = (({event,resolve}) => {
 
-    event.cookies.set('test','value', {path: '/'})
+    const token = event.cookies.get('token');
+
+    if(!token && event.route.id?.startsWith('/(app)')) {
+        redirect(307, '/signin')
+    }
+
+    event.locals.user = token ? {name: 'John', id: 1} : null;
 
     return resolve(event);
 })
