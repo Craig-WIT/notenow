87f7281bc5d90ece55b7ebdcdd33db2e3709d700
Craig-WIT <20095351@mail.wit.ie>
2025-09-08 08:59:09 +0100

    Change to progress bar

--- src/routes/(marketing)/+layout.server.ts
+++ src/routes/(marketing)/+layout.server.ts
@@ -1,7 +1,8 @@
 import type { LayoutServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({locals}) => {
     return {
+        user: locals.user,
         marketingLayoutData: 'Some data on marketing'
     };
 }) satisfies LayoutServerLoad;
