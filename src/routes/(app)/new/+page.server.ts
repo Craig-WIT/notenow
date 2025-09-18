cc76f9ae99652ce07e8f585d8dc1cafa4cd58217
Craig-WIT <20095351@mail.wit.ie>
2025-09-18 21:25:24 +0100

    Inserting a new workspace in the DB

--- src/routes/(app)/new/+page.server.ts
+++ src/routes/(app)/new/+page.server.ts
@@ -1,5 +1,8 @@
-import { fail, redirect, type Actions } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { db } from '$lib/server/db';
+import { roles, workspaceAccess, workspaces } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
 
 export const load = (async ({ locals }) => {
 	if (!locals.session) {
@@ -11,22 +14,41 @@ export const load = (async ({ locals }) => {
 export const actions = {
 	createWorkspace: async ({ request, locals }) => {
 		if (!locals.session) {
-			return fail(401, { message: 'Unauthorised' });
+			return fail(401, { message: 'Unauthorized', name: '' });
 		}
-
 		const data = await request.formData();
 		const name = data.get('name');
-
 		if (!name) {
-			return fail(400, { message: 'Name is required' });
+			return fail(400, { message: 'Name is Required!', name: '' });
 		}
-
 		if (name.toString().length < 4) {
-			return fail(400, { message: 'Name is too short' });
+			return fail(400, { message: 'Name is too short!', name });
 		}
-
-		return {
-			message: 'Workspace created successfully!'
-		};
+		let _newWorkspace;
+		try {
+			_newWorkspace = await db.transaction(async (tx) => {
+				const [newWorkspace] = await tx
+					.insert(workspaces)
+					.values({
+						name: name.toString()
+					})
+					.returning({ id: workspaces.id });
+				if (!newWorkspace) throw new Error('Workspace creation failed!');
+				const [adminRole] = await tx.select().from(roles).where(eq(roles.name, 'admin'));
+				if (!adminRole) throw new Error('Admin role not found');
+				await tx.insert(workspaceAccess).values({
+					userId: locals.session.user.id,
+					workspaceId: newWorkspace.id,
+					roleId: adminRole.id
+				});
+				return newWorkspace;
+			});
+		} catch (error) {
+			// Report
+			console.log(error);
+			return fail(500, { message: 'An error has occurred!' });
+		}
+		redirect(303, `/w/${_newWorkspace.id}`);
+		// return { message: 'Workspace created successfully!' };
 	}
 } satisfies Actions;
--- src/routes/(app)/new/+page.svelte
+++ src/routes/(app)/new/+page.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import { CircleCheck, CircleX } from '@lucide/svelte';
 	import type { PageProps } from './$types';
-	import { page } from '$app/state';
 	import FormMessage from '$components/FormMessage.svelte';
 
 	let { data, form }: PageProps = $props();
 </script>
 
-<div class="card prose w-full rounded-md border-1 border-base-300 bg-base-200">
-	<div class="card-body">
-		{#if form?.message}
-			<FormMessage message={form?.message} />
-		{/if}
-		<form method="POST" action="?/createWorkspace">
-			<label for="ws-name">Name</label>
-			<input id="ws-name" name="name" class="input w-full rounded-md bg-base-200" type="text" />
-			<button class="btn mt-4 w-full rounded-md btn-primary" type="submit">Submit</button>
-		</form>
+<div class="p-5">
+	<div class="card prose w-full rounded-md border-1 border-base-300 bg-base-200">
+		<div class="card-body">
+			{#if form?.message}
+				<FormMessage message={form.message} />
+			{/if}
+			<form method="POST" action="?/createWorkspace">
+				<label for="ws-name">Name</label>
+				<input
+					id="ws-name"
+					value={form?.name}
+					name="name"
+					class="input w-full rounded-md bg-base-200"
+					type="text"
+				/>
+				<button class="btn mt-4 w-full rounded-md btn-primary" type="submit">Submit</button>
+			</form>
+		</div>
 	</div>
 </div>
