2352313d7e1361da0cb120fb949d934fcd6e718d
Craig-WIT <20095351@mail.wit.ie>
2025-11-24 12:35:16 +0000

    Implementing the Register form action

--- src/routes/(auth)/[authType=authType]/+page.server.ts
+++ src/routes/(auth)/[authType=authType]/+page.server.ts
@@ -1,9 +1,12 @@
-import { redirect } from '@sveltejs/kit';
+import { redirect, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { superValidate } from 'sveltekit-superforms';
+import { fail, message, superValidate } from 'sveltekit-superforms';
 import { userLoginSchema } from '$lib/schemas/login-schema';
 import { zod } from 'sveltekit-superforms/adapters';
 import { userRegisterSchema } from '$lib/schemas/register-schema';
+import { auth } from '$lib/server/auth';
+import { getGravatarUrl } from '$lib/utils';
+import { APIError } from 'better-auth';
 
 export const load = (async ({ locals }) => {
 	if (locals.session) {
@@ -11,6 +14,49 @@ export const load = (async ({ locals }) => {
 	}
 	return {
 		loginForm: await superValidate(zod(userLoginSchema)),
-		registerForm: await superValidate(zod(userRegisterSchema))
+		registerForm: await superValidate(
+			{
+				email: 'test@test.com',
+				name: 'Test',
+				username: 'test',
+				password: '123456789',
+				confirmPassword: '123456789'
+			},
+			zod(userRegisterSchema)
+		)
 	};
 }) satisfies PageServerLoad;
+
+export const actions = {
+	register: async ({ request }) => {
+		const form = await superValidate(request, zod(userRegisterSchema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		const { username, email, name, password } = form.data;
+
+		try {
+			const res = await auth.api.signUpEmail({
+				headers: request.headers,
+				body: {
+					username,
+					email,
+					name,
+					password,
+					image: getGravatarUrl(email)
+				}
+			});
+			console.log(res);
+			return message(form, 'A Confirmation Email has been sent to verify your account');
+		} catch (error) {
+			let errorMessage = 'An error has occured';
+			console.log(error);
+			if (error instanceof APIError) {
+				const duplicateUsername = error?.body?.details?.constraint_name === 'users_username_unique';
+				errorMessage = duplicateUsername ? 'Username must be unique' : error.message;
+			}
+			return message(form, errorMessage, { status: 400 });
+		}
+	}
+} satisfies Actions;
