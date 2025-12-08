4c5cc54e41488429b9649fe1aa560884b82c8aa3
Craig-WIT <20095351@mail.wit.ie>
2025-12-08 22:32:36 +0000

    Protecting page route and changing UI markup

--- src/routes/(app)/(workspace)/p/[pid]/+page.server.ts
+++ src/routes/(app)/(workspace)/p/[pid]/+page.server.ts
@@ -2,40 +2,58 @@ import { db } from '$lib/server/db';
 import { notes, pages, users } from '$lib/server/db/schema';
 import { eq, desc } from 'drizzle-orm';
 import type { PageServerLoad } from './$types';
+import { getWorkspaceAccess, getWorkspaceIDFromPageId } from '$lib/server/db/utils';
+import { requireLogin } from '$lib/server/db/utils';
+import { subject } from '@casl/ability';
+import { redirect } from '@sveltejs/kit';
 
-export const load = (async ({params}) => {
-    // TODO: Check user is logged in and page access etc
+export const load = (async ({ params }) => {
+	const { user } = requireLogin();
 
-    async function getPage() {
-        const page = await db
-        .select()
-        .from(pages)
-        .where(eq(pages.id, params.pid))
-        .limit(1)
-        .then((r) => r[0]);
+	const workspaceId = await getWorkspaceIDFromPageId(params.pid);
 
-        return page;
-    }
+	const { workspaceAccess, pageAccess, ability } = await getWorkspaceAccess({
+		user,
+		workspaceId,
+		pageId: params.pid
+	});
 
-    async function getNotes() {
-        const _notes = await db
-        .select({
-            id: notes.id,
-            content: notes.content,
-            userId: notes.userId,
-            pageId: notes.pageId,
-            username: users.name,
-        })
-        .from(notes)
-        .innerJoin(users, eq(notes.userId, users.id))
-        .where(eq(notes.pageId, params.pid))
-        .orderBy(desc(notes.createdAt));
+	if (ability.cannot('read', subject('Page', { id: params.pid }))) {
+		redirect(307, '/access-denied');
+	}
 
-        return _notes;
-    }
+	async function getPage() {
+		const page = await db
+			.select()
+			.from(pages)
+			.where(eq(pages.id, params.pid))
+			.limit(1)
+			.then((r) => r[0]);
 
-    return {
-        notes: getNotes(),
-        page: await getPage(),
-    };
+		return page;
+	}
+
+	async function getNotes() {
+		const _notes = await db
+			.select({
+				id: notes.id,
+				content: notes.content,
+				userId: notes.userId,
+				pageId: notes.pageId,
+				username: users.name
+			})
+			.from(notes)
+			.innerJoin(users, eq(notes.userId, users.id))
+			.where(eq(notes.pageId, params.pid))
+			.orderBy(desc(notes.createdAt));
+
+		return _notes;
+	}
+
+	return {
+		notes: getNotes(),
+		page: await getPage(),
+		workspaceAccess,
+		pageAccess
+	};
 }) satisfies PageServerLoad;
